47f97e53c7533654f5d4aec9012994163280b929
Josh Graff <jgraff@verivo.com>
2013-11-27 10:24:50 -0500

    Taste profile logic added

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -21,7 +21,9 @@ angular.module('myApp.controllers', []).
 function MusicCtrl($scope, $http) {
     $scope.result = 'No artists found';
     $scope.artists = null;
+    $scope.topArtists = null;
     $scope.profileID = null;
+    $scope.playlistSongs = [];
 
 
     $scope.makePlaylist = function(){
@@ -51,9 +53,17 @@ function MusicCtrl($scope, $http) {
             Library.forCurrentUser().load("artists").done(function(library) {
                 library.artists.snapshot().done(function(snapshot) {
                     $scope.result = snapshot.length;
-                    $scope.artists = snapshot;
+                    $scope.artists = snapshot["_uris"];
                 })
             })
+        });
+
+        require(['$api/toplists#Toplist'], function(Toplist) {
+            var toplist = Toplist.forCurrentUser();
+            toplist.artists.snapshot().done(function(userTopArtists){
+                $scope.topArtists = userTopArtists;
+            })
+        });
 
             /**
             models.player.load('track').done(function(player) {
@@ -66,37 +76,118 @@ function MusicCtrl($scope, $http) {
                     });
                 }
             });   */
+    }
+
+    function getData(){
+        var artistJson = [];
+        for(var i =0; i<$scope.artists.length; i++){
+            var artistUID = $scope.artists[i].replace("spotify","spotify-WW");
+
+            artistJson.push({item: {item_id:"artistID-"+artistUID, artist_id:artistUID}});
+        }
+        alert(JSON.stringify(artistJson));
+        return {data: JSON.stringify(artistJson)};
+    }
+
+    $scope.getRandomPlaylist = function() {
+        var suggestions = null;
+        $http({
+            method:'GET',
+            url: 'http://developer.echonest.com/api/v4/playlist/static?api_key=VOW1HBCF5U0DHVUDM',
+            params: { 'seed_catalog': $scope.profileID, 'format': 'json', 'results': 30, 'type':'catalog-radio'}
+            //'data':  { 'format':'json', 'data_type': 'json', 'id': $scope.profileID, 'data': JSON.stringify([{"item":{"item_id":"royks","artist_name":"Royksopp"}},{"item":{"item_id":"inter","artist_name":"Interpol"}}])},
         })
 
+            .success(function(data){
+                suggestions = data;
+            })
+            .error(function(err){
+                alert("fail "+err);
+            })
+
+
+
+
+        //http://developer.echonest.com/api/v4/playlist/static?api_key=VOW1HBCF5U0DHVUDM&seed_catalog=CADPRLD14281476904&format=json&results=30&type=catalog-radio
     }
+
     $scope.sendTastes = function() {
         if($scope.artists != null){ //Need artists
+            //var stringData = $.param({data: JSON.stringify([{item:{item_id:"royks",artist_name:"Royksopp"}},{item:{item_id:"inter",artist_name:"Interpol"}}])})
+            var url = 'http://developer.echonest.com/api/v4/catalog/update?api_key=VOW1HBCF5U0DHVUDM';
+            //alert("string data " +stringData);
+
+
+            var stringData = $.param(getData());
+            //return;
+
+
+
+
+
             $http({
-                'method':'POST',
-                'url': 'http://developer.echonest.com/api/v4/catalog/update?api_key=VOW1HBCF5U0DHVUDM',
-                'params': { 'format':'json', 'data_type': 'json', 'id': $scope.profileID},
-                'data': JSON.stringify([{
-                    "item":
-                    {
-                        "item_id": "royks",
-                        "artist_name": "Royksopp"
-                    }
-                },
-                    {
-                        "item":
-                        {
-                            "item_id": "inter",
-                            "artist_name": "Interpol"
-                        }
-                    }]),
-                'cache':false
+                method:'POST',
+                url: 'http://developer.echonest.com/api/v4/catalog/update?api_key=VOW1HBCF5U0DHVUDM',
+                params: { 'format':'json', 'data_type': 'json', 'id': $scope.profileID},
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                data: stringData
+                //'data':  { 'format':'json', 'data_type': 'json', 'id': $scope.profileID, 'data': JSON.stringify([{"item":{"item_id":"royks","artist_name":"Royksopp"}},{"item":{"item_id":"inter","artist_name":"Interpol"}}])},
             })
+
+            /*$.ajax(
+                {
+                    type:'POST',
+                    url: 'http://developer.echonest.com/api/v4/catalog/update?api_key=VOW1HBCF5U0DHVUDM&data_type=json&format=json&id=CADPRLD14281476904',
+                    //contentType: 'application/json; charset=utf-8',
+                    //params: { 'format':'json', 'data_type': 'json', 'id': $scope.profileID },
+                    data: stringData
+                    //'data':  { 'format':'json', 'data_type': 'json', 'id': $scope.profileID, 'data': JSON.stringify([{"item":{"item_id":"royks","artist_name":"Royksopp"}},{"item":{"item_id":"inter","artist_name":"Interpol"}}])},
+                })*/
+
                 .success(function(data){
-                    alert("success post!");
+                    alert("success post! "+JSON.stringify(data));
                 })
                 .error(function(err){
                     alert("fail "+err);
                 })
+
+            /*var form = document.createElement("form");
+            form.setAttribute("method", "post");
+            form.setAttribute("action", url);
+
+            var hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type","hidden");
+            hiddenField.setAttribute("name","format");
+            hiddenField.setAttribute("value","json");
+
+            form.appendChild(hiddenField);
+
+            var hiddenField2 = document.createElement("input");
+            hiddenField2.setAttribute("type","hidden");
+            hiddenField2.setAttribute("name","data_type");
+            hiddenField2.setAttribute("value","json");
+
+            form.appendChild(hiddenField2);
+
+            var hiddenField3 = document.createElement("input");
+            hiddenField3.setAttribute("type","hidden");
+            hiddenField3.setAttribute("name","id");
+            hiddenField3.setAttribute("value",$scope.profileID);
+
+            form.appendChild(hiddenField3);
+
+            var hiddenField4 = document.createElement("input");
+            hiddenField4.setAttribute("type","hidden");
+            hiddenField4.setAttribute("name","data");
+            hiddenField4.setAttribute("value",stringData);
+
+            form.appendChild(hiddenField4);
+
+
+            document.body.appendChild(form);
+            form.submit();
+            */
+
         }
     }
 }
