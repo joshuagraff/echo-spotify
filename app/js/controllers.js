decc1d48357d66672bb8b2e4c6a2dab2eee83fbd
Josh Graff <jgraff@verivo.com>
2013-11-27 20:30:39 -0500

    Changed taste profile to songs not artists, basic PoC fleshed out

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -20,7 +20,7 @@ angular.module('myApp.controllers', []).
 
 function MusicCtrl($scope, $http) {
     $scope.result = 'No artists found';
-    $scope.artists = null;
+    $scope.tracks = null;
     $scope.topArtists = null;
     $scope.profileID = null;
     $scope.playlistSongs = [];
@@ -31,7 +31,7 @@ function MusicCtrl($scope, $http) {
         $http({
             'method':'POST',
             'url': 'http://developer.echonest.com/api/v4/catalog/create?api_key=VOW1HBCF5U0DHVUDM',
-            'params': { 'type': "artist", 'format':'json', 'name': "echoSpotify_test_artist_catalog"},
+            'params': { 'format':'json', 'name': "echoSpotify_dev_tracks"},
             'data': {},
             'headers': {"Content-Type":"multipart/form-data"},
             'cache':false
@@ -50,10 +50,11 @@ function MusicCtrl($scope, $http) {
             })
 
         require(['$api/library#Library'], function(Library) {
-            Library.forCurrentUser().load("artists").done(function(library) {
-                library.artists.snapshot().done(function(snapshot) {
+            Library.forCurrentUser().load("tracks").done(function(library) {
+                library.tracks.snapshot().done(function(snapshot) {
+                    alert(JSON.stringify(snapshot));
                     $scope.result = snapshot.length;
-                    $scope.artists = snapshot["_uris"];
+                    $scope.tracks = snapshot["_uris"];
                 })
             })
         });
@@ -79,27 +80,49 @@ function MusicCtrl($scope, $http) {
     }
 
     function getData(){
-        var artistJson = [];
-        for(var i =0; i<$scope.artists.length; i++){
-            var artistUID = $scope.artists[i].replace("spotify","spotify-WW");
+        var trackJson = [];
+        for(var i =0; i<$scope.tracks.length; i++){
+            var trackUID = $scope.tracks[i].replace("spotify","spotify-WW");
 
-            artistJson.push({item: {item_id:"artistID-"+artistUID, artist_id:artistUID}});
+            trackJson.push({item: {item_id:"trackID-"+trackUID, track_id:trackUID}});
         }
-        alert(JSON.stringify(artistJson));
-        return {data: JSON.stringify(artistJson)};
+        alert(JSON.stringify(trackJson));
+        return {data: JSON.stringify(trackJson)};
     }
 
     $scope.getRandomPlaylist = function() {
-        var suggestions = null;
         $http({
             method:'GET',
-            url: 'http://developer.echonest.com/api/v4/playlist/static?api_key=VOW1HBCF5U0DHVUDM',
-            params: { 'seed_catalog': $scope.profileID, 'format': 'json', 'results': 30, 'type':'catalog-radio'}
-            //'data':  { 'format':'json', 'data_type': 'json', 'id': $scope.profileID, 'data': JSON.stringify([{"item":{"item_id":"royks","artist_name":"Royksopp"}},{"item":{"item_id":"inter","artist_name":"Interpol"}}])},
+            url: 'http://developer.echonest.com/api/v4/playlist/static?api_key=VOW1HBCF5U0DHVUDM&bucket=tracks',
+            params: { 'seed_catalog': $scope.profileID, 'format': 'json', 'results': 50, 'type':'catalog-radio', 'bucket':'id:spotify-WW', 'adventurousness':.9}
         })
 
             .success(function(data){
-                suggestions = data;
+                alert(JSON.stringify(data));
+                var songs = data["response"]["songs"];
+
+                for(var i=0; i<songs.length; i++){
+                    if(songs[i]["tracks"][0]){
+                        $scope.playlistSongs.push(songs[i]["tracks"][0]["foreign_id"].replace("spotify-WW","spotify"));
+                    }
+
+                }
+                alert($scope.playlistSongs);
+                if($scope.playlistSongs.length != 0){
+                    require(['$api/models'], function(models) {
+                        models.Playlist.create("Discover Playlist").done(function(playlist) {
+                            playlist.load("tracks").done(function(playlist) {
+                                var tracks = [];
+                                for (var i = 0; i < songs.length; i++) {
+                                    if($scope.playlistSongs[i]){
+                                        tracks.push(models.Track.fromURI($scope.playlistSongs[i]));
+                                    }
+                                }
+                                playlist.tracks.add(tracks);
+                            });
+                        });
+                    });
+                }
             })
             .error(function(err){
                 alert("fail "+err);
@@ -112,18 +135,13 @@ function MusicCtrl($scope, $http) {
     }
 
     $scope.sendTastes = function() {
-        if($scope.artists != null){ //Need artists
+        if($scope.tracks != null){ //Need artists
             //var stringData = $.param({data: JSON.stringify([{item:{item_id:"royks",artist_name:"Royksopp"}},{item:{item_id:"inter",artist_name:"Interpol"}}])})
             var url = 'http://developer.echonest.com/api/v4/catalog/update?api_key=VOW1HBCF5U0DHVUDM';
             //alert("string data " +stringData);
 
 
             var stringData = $.param(getData());
-            //return;
-
-
-
-
 
             $http({
                 method:'POST',
@@ -131,7 +149,6 @@ function MusicCtrl($scope, $http) {
                 params: { 'format':'json', 'data_type': 'json', 'id': $scope.profileID},
                 headers: {'Content-Type': 'application/x-www-form-urlencoded'},
                 data: stringData
-                //'data':  { 'format':'json', 'data_type': 'json', 'id': $scope.profileID, 'data': JSON.stringify([{"item":{"item_id":"royks","artist_name":"Royksopp"}},{"item":{"item_id":"inter","artist_name":"Interpol"}}])},
             })
 
             /*$.ajax(
